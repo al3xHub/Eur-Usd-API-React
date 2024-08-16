a789acdbd759b560da5df25a0227eb79bdc9f3ec
al3xHub <alejandrobueno92@hotmail.com>
2024-08-16 14:10:23 +0200

    connected app with API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
 
-  const [resultado, setResultado] = useState(null)
+  const [valor, setValor] = useState(null)
+
+  const eurRef = useRef();
+  const resultRef = useRef();
+
+  useEffect(() => {
+
+    const apiKey = '235acb0c487ae67009d0cc9d'
+
+    const callApi = async () => {
+      try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/EUR`);
+        const data = await response.json();
+        setValor(data.conversion_rates.USD);
+      } catch {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    callApi();
+
+  }, [])
 
   function buttonClicked() {
 
-    const euro = document.querySelector("input").value;
-    const resultado = euro * 1.08;
+    const eurosValor = parseFloat(eurRef.current.value);
+    const dolares = eurosValor * valor
 
-    setResultado(resultado);
+    resultRef.current.innerHTML = dolares.toFixed(2)+"$";
 
   }
 
@@ -18,9 +39,9 @@ function App() {
     <>
 
       <h1>Euro Dollar Conversor</h1>
-      <input className='center-elements' type="number" placeholder='EUR amount' />
-      <button className='center-elements' onClick={buttonClicked}>Convert EUR/USD</button>
-      <h2>{resultado != null ? resultado + "$" : ""}</h2>
+      <input className='center-elements' type="number" placeholder='EUR amount' ref={eurRef} />
+      <button className='center-elements'onClick={buttonClicked}>Convert EUR/USD</button>
+      <h2 ref={resultRef}></h2>
 
     </>
   )
